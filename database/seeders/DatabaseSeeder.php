19b546b0a02dd042287df3d022e9c126b06a3ba9
Jose Perez <joserph.a@gmail.com>
2023-07-14 22:09:08 -0500

    Base laravel 10 listo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(SeederTablePermissions::class);
+        $this->call(SuperAdminSeeder::class);
     }
 }
